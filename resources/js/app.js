0d260a8a61aa05800eb5f8064d2f6679f3b9a829
Rocinante9999 <najaardiansyah@gmail.com>
2025-11-29 14:23:29 +0700

    Fix Ziggy import for Vercel build

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,7 @@ import '../css/app.css';
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { Ziggy } from './ziggy'; // Mengarah ke file ziggy.js di folder yang sama
 import Alpine from 'alpinejs';
 
 // PERBAIKAN: Gunakan nama aplikasi Anda sebagai fallback
